c5e4cfc5acde31d36f2b4f5ff387d86de4d131bf
Geovanny <geovanny.alcon2@gmail.com>
2024-10-26 20:44:45 -0600

    Particion de espacios

--- src/App.js
+++ src/App.js
@@ -1,47 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Section from './Section'
+import Notes from './Notes'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Note">
+      //**Henry */
+      <Section></Section>
+
+      
+      //** Geo */
+      <Notes></Notes>
     </div>
   );
 }
 
-//** Henry */
-function Henry() {
-  return(
-    <div className="App">
-      <body>
-      <p>
-          XDXDXD
-      </p>
-      </body>
-    </div>
 
-
-  );
-
-}
-
-//** Geo */
-function Geo() {
-
-
-}
 
 export default App;
---
+++ src/Notes.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Notes.css';
+
+function Notes() {
+    <div className="Prueba">
+        <h1>Notas Geo</h1>
+    </div>
+}
+
+export default Notes;
