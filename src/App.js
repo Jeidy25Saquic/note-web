8d91130fbeebdf45d0dfcaa469f045961d60805b
Geovanny <geovanny.alcon2@gmail.com>
2024-10-27 13:36:23 -0600

    Correcciones de UI

--- src/App.js
+++ src/App.js
@@ -3,17 +3,13 @@ import Section from './Section'
 import BarraDeFunciones from './BarraDeFunciones'; // Importa la barra de herramientass
 import Notes from './Notes';
 import Panel from './Panel';
+
 function App() {
   return (
     <div className = "Principal">
       {/* Barra de tareas, colocada sobre los botones existentes */}
       <BarraDeFunciones />
       <Section  />
-      
-      
-      
-      
-      //Geo
       <Notes></Notes>
     </div>
   );
