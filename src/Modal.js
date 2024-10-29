eba5500ae17ffeddac4667701e514989497d5618
Jennifer Saquic <jenntecum2001@gmail.com>
2024-10-29 17:07:20 -0600

    Modal

---
+++ src/Modal.js
@@ -0,0 +1,22 @@
+// Modal.js
+// Modal.js
+import React from 'react';
+import './Modal.css';
+
+const Modal = ({ isOpen, onClose, content }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <button className="close-button" onClick={onClose}>X</button>
+        <div className="modal-body">
+          <h2>Detalles de la Nota</h2>
+          <p>{content}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/Notes.js
+++ src/Notes.js
@@ -1,11 +1,23 @@
 // NotaComponent.js
-import React from 'react';
-import './Notes.css'; // Archivo de estilos para NotaComponent
+import React, { useState } from 'react';
+import './Notes.css';
+import Modal from './Modal';
 
-const NotaComponent = ({ content }) => (
-  <div className="note">
-    <p>{content}</p>
-  </div>
-);
+const NotaComponent = ({ content }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
+  return (
+    <div>
+      <div className="note" onClick={openModal}>
+        <p>{content}</p>
+      </div>
+      {/* Modal solo se muestra si isModalOpen es true */}
+      <Modal isOpen={isModalOpen} onClose={closeModal} content={content} />
+    </div>
+  );
+};
 
 export default NotaComponent;
